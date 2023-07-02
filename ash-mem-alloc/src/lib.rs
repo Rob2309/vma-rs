ece48a8ec00626d6b459945b233383b2234d4f9b
Robin Quint <rob2309@hotmail.de>
2023-07-02 12:57:56 +0200

    Inlined crate doc to allow publishing

--- ash-mem-alloc/src/lib.rs
+++ ash-mem-alloc/src/lib.rs
@@ -1,4 +1,20 @@
-#![doc = include_str!("../../README.md")]
+//! # ash-mem-alloc
+//! 
+//! This crate provides auto generated [ash](https://github.com/ash-rs/ash)-style bindings to the beloved [Vulkan Memory Allocator](https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator).
+//! 
+//! Every VMA struct has an associated `builder` just like ash structs:
+//! ```rust ignore
+//! use ash_mem_alloc::vma;
+//! 
+//! let info = vma::AllocatorCreateInfo::builder()
+//!     .device(device.handle())
+//!     .instance(instance.handle())
+//!     .physical_device(physical_device)
+//!     .vulkan_functions(&functions);
+//! ```
+//! 
+//! While certain convenience features are implemented, all functions are unsafe and operate on raw `Vma` and `Vk` handles, just like in `ash`.
+//! For the most part, functions will generally behave as one would expect from identical `ash` functions.
 
 #[allow(clippy::missing_safety_doc)]
 mod bindings;
